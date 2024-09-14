1c27cef6eb8383e3afcbbe143d90bb46bce7d760
patd <pdoran@gmail.com>
2024-09-14 15:43:47 -0400

    WIP building pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'PatrickDoran.com';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Patrick Doran';
+export const SITE_DESCRIPTION = 'Learn about Patrick Doran, a developer and designer near Raleigh, North Carolina';
